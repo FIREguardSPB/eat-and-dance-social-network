6644a5d07cc9f1fbfb788705048319e02b8e13a7
llixgrijb <excellex@ya.ru>
2020-10-28 17:49:59 +0300

    deleted sessions

---
+++ models/posts.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+// модель постов пользователей
+const postSchema = new mongoose.Schema({
+  postText: { type: String, required: true }, //текст поста
+  postDate: { type: Date, default: Date.now }, //дата
+  postAuthor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // id автора
+  likersRef: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // список лайкнувших
+  theme: { type: mongoose.Schema.Types.ObjectId, ref: 'Theme' }, // тема в которой пост
+  commentRef: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // список комментов
+});
+
+
+const Post = new mongoose.model('Post', postSchema);
+module.exports = Post  
--- models/users.js
+++ models/users.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  regdate: { type: Date },
+  userpic: { url },
+  postsRef: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  likesRef: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  skillsFood: [{type: String}],
+  skillsDance: [{type: String}]
+
 });
 
 module.exports = mongoose.model('User', userSchema);
