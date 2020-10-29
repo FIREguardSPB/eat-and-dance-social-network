328c1b54d5c74597fd3b6be9ac2e790878a1bdef
llixgrijb <excellex@ya.ru>
2020-10-30 00:18:39 +0300

    showposts theme method

--- models/themes.js
+++ models/themes.js
@@ -1,20 +1,16 @@
+const { localsAsTemplateData } = require('hbs');
 const mongoose = require('mongoose')
 const Post = require('./posts')
 // модель тем
 const themeSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true}, // название темы
+  name: { type: String, required: true, unique: true }, // название темы
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // посты в теме
 });
 
-themeSchema.methods.showPosts = async function(){
-  const posts = this.posts.populate('postText')
-  console.log(posts);
-// for(post in posts){
-//   const newp = await Post.findOne({_id: posts[post]})
+themeSchema.methods.showPosts = async function () {
+  const id = this._id
+  return await Post.find({ theme: id })
 }
-  
 
 const Theme = new mongoose.model('Theme', themeSchema);
-
->>>>>>> main
 module.exports = mongoose.model('Theme', themeSchema);
