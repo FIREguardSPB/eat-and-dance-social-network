ef3e5a9467d55986a1c1624323040a21d2024ebc
llixgrijb <excellex@ya.ru>
2020-10-29 22:59:05 +0300

    8

--- models/themes.js
+++ models/themes.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
+const Post = require('./posts')
 // модель тем
 const themeSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true}, // название темы
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // посты в теме
 });
 
+themeSchema.methods.showPosts = async function(){
+  const posts = this.posts
+for(post in posts){
+  const newp = await Post.findOne({_id: posts[post]})
+}
+  
+}
+
 const Theme = new mongoose.model('Theme', themeSchema);
-module.exports = Theme  
+
+module.exports = mongoose.model('Theme', themeSchema);
