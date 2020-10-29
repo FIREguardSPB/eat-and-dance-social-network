bacd013cf0f0392c699d3e14caf95b8a152f6c6e
KonstantinChebotarev <konstantin.spb@yandex.ru>
2020-10-29 17:30:09 +0300

    переходы на dance и food

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const User = require("../models/users");
 const saltRounds = 10;
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("main");
+router.get("/", isLogin, (req, res) => {
+  res.render("main", {Inlogin: res.locals.status});
 });
 
 router
@@ -50,14 +50,6 @@ router
     }
   });
 
-// router.get("/registration", (req, res) => {
-//   const { user } = req.session;
-//   if (req.session.user) {
-//     res.render("signup", { name: user.username });
-//   } else {
-//     res.redirect("/login");
-//   }
-// });
 
 router.get("/logout", async (req, res, next) => {
   if (req.session.user) {
