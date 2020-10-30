694115a927265a905c8ec2062517d1c7e198e7b1
Anton <stells@ngs.ru>
2020-10-30 13:04:43 +0300

    6

--- routes/dance.js
+++ routes/dance.js
@@ -1,108 +1,45 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { sessionChecker } = require("../middleware/auth");
 const User = require("../models/users");
-const Post = require('../models/posts')
-const Theme = require('../models/themes')
+const Post = require("../models/posts");
+const Theme = require("../models/themes");
 
 const saltRounds = 10;
-// const app 
+// const app
 // app.use(express.urlencoded({extended: true}))
 
 //генерируем главную страницу раздела "DANCE"
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const name = await Theme.find();
 
-  const name = await Theme.find()
+  // console.log(name)
+  res.render("index_dance", { name: name, _id: name });
+});
 
-  console.log(name)
-  res.render('index_dance', { name: name, _id: name })
-})
-// router.get('/post-edit-form', (req, res) => {
-//   res.render()
-// })
 
 /* create newPost. */
-router.get('/ajax-create-post', (req, res) => res.render('newPost'))
-router.post('/ajax-create-post', async function (req, res) {
-  const { newPost } = req.body
-  console.log(newPost)
-  const user = await User.findOne({ username: 'Davonte16' })
-  await user.createpost(newPost, 'placeat')
-
-  res.redirect('/dance')
-
+router.get("/ajax-create-post", (req, res) => res.render("newPost"));
+router.post("/ajax-create-post", async function (req, res) {
+  const { newPost } = req.body;
+  const user = await User.findOne({ username: "Davonte16" });
+  await user.createpost(newPost, "placeat");
+  res.redirect("/dance");
 });
+
 // Отображение постов
-router.get('/post', async function (req, res) {
-const nameTheme = req.query.ID
-const postsOfThem = await Theme.findOne({_id: nameTheme})
-// console.log(postsOfThem)
-//Массив ID постов по выбранной теме
-// res.json(postsOfThem.posts)
+router.get("/post", async function (req, res) {
+  //Получаем ID темы
+  const nameTheme = req.query.ID;
+  //Массив ID постов по выбранной теме
+  const postsOfThem = await Theme.findOne({ _id: nameTheme });
+  const posts = postsOfThem.posts;
 
-const posts = postsOfThem.posts
+  //Показать текст постов темы.
+  const viewPosts = await postsOfThem.showPosts();
+  res.render("dance_posts", { viewPosts});
+  
+});
 
-//Показать посты темы
-const viewPosts = await postsOfThem.showPosts()
-console.log(viewPosts[0].postText)
-
-let p 
-// for (i=0; i<posts.length; i++){
-//   p = await Post.findOne({_id: posts[i]})
-
-//   // console.log(p[postText])
-//   // console.log('\n',p)
-//   // console.log(await Post.findOne({_id: posts[i]}))
-// }
-// console.log(p.postText)
-// textViewPosts = posts.map(el => {textViewPosts.push(Post.findOne({_id: el}))})
-res.json(viewPosts[0].postText)
-
-})
-
-
-
-
-
-// router.post('/ajax-create-post', async function(req, res) {
-//   // const {post} = req.body
-//   // const newPost = new Post({
-//   //   completed: false,
-//   //   text: todo
-//   // })
-//   // await newPost.save()
-
-//   const {text} = req.body
-//   console.log(text);
-//   res.json({text, success: true})
-
-// });
-
-//===================.............>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-// const Todo = require('../models/Todo')
-
-// const router = express.Router()
-
-// router.post('/', async (req ,res) => {
-//   const { todo } = req.body
-//   const newTodo = new Todo({
-//     completed: false,
-//     text: todo
-//   })
-//   await newTodo.save()
-//   res.redirect('/')
-// })
-
-// router.post('/ajax', async (req, res) => {
-//   const {todo} = req.body
-//   const newTodo = new Todo({
-//     completed: true,
-//     text: todo
-//   })
-//   await newTodo.save()
-//   res.json({newTodo, success: true})
-// })
-
-module.exports = router
+module.exports = router;
